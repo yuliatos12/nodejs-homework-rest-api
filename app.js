ebd43cb8418138b42d8f27cbe0591e6b0f7daf4c
yuliatos12 <yaremenko.yulia8@gmail.com>
2023-11-29 19:01:44 +0200

    +

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const logger = require('morgan')
 const cors = require('cors')
 
+
 const contactsRouter = require('./routes/api/contacts')
 
 const app = express()
@@ -11,9 +12,7 @@ const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
 app.use(logger(formatsLogger))
 app.use(cors())
 app.use(express.json())
-
 app.use('/api/contacts', contactsRouter)
-
 app.use((req, res) => {
   res.status(404).json({ message: 'Not found' })
 })
