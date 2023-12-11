a9820862dd31fca2f4415fd3ab2fb8fe95fef603
yuliatos12 <yaremenko.yulia8@gmail.com>
2023-12-11 19:28:49 +0200

    +

--- controllers/auth.js
+++ controllers/auth.js
@@ -54,7 +54,7 @@ const login = async(req, res) => {
 const getCurrent = async(req, res) => {
     const {email, subscription} = req.user;
 
-    register.json({
+    res.json({
         email,
         subscription,
     })
